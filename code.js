898e798725989febdf576be1acc6fa685c54dcb0
isaac lee <isaac@paperly.us>
2019-12-26 12:42:34 +0900

    text alignment

--- code.js
+++ code.js
@@ -45,18 +45,16 @@ function styledComponent(node) {
     let text = '';
     let tag = 'div';
     if (node.type === 'TEXT') {
-        tag = 'p';
+        tag = 'div';
     }
     else if (isSvgNode(node)) {
         tag = 'img';
     }
     text += `const ${node.name} = styled.${tag}` + "`\n";
-    text += `  width: ${node.width};\n`;
-    text += `  height: ${node.height};\n`;
     if (node.type === 'TEXT') {
         const fillColor = getFillColor(node);
         if (fillColor) {
-            text += `  color: ${fillColor}};\n`;
+            text += `  color: ${fillColor};\n`;
         }
     }
     if (node.type === 'TEXT' || node.type === 'FRAME') {
@@ -94,20 +92,20 @@ function styledComponent(node) {
         }
         // add padding
         if (node.verticalPadding) {
-            text += `  padding-top: ${node.verticalPadding};\n`;
-            text += `  padding-bottom: ${node.verticalPadding};\n`;
+            text += `  padding-top: ${node.verticalPadding}px;\n`;
+            text += `  padding-bottom: ${node.verticalPadding}px;\n`;
         }
         if (node.horizontalPadding) {
-            text += `  padding-left: ${node.horizontalPadding};\n`;
-            text += `  padding-right: ${node.horizontalPadding};\n`;
+            text += `  padding-left: ${node.horizontalPadding}px;\n`;
+            text += `  padding-right: ${node.horizontalPadding}px;\n`;
         }
         // Auto layout: item spacing to margin
-        if (node.parent && node.parent.type === 'FRAME' && node.parent.layoutMode === 'HORIZONTAL') {
-            text += `  margin: ${node.parent.itemSpacing}px 0px;\n`;
-        }
-        if (node.parent && node.parent.type === 'FRAME' && node.parent.layoutMode === 'VERTICAL') {
-            text += `  margin: 0px ${node.parent.itemSpacing}px;\n`;
-        }
+        // if (node.parent && node.parent.type === 'FRAME' && node.parent.layoutMode === 'HORIZONTAL') {
+        //   text += `  margin: ${node.parent.itemSpacing}px 0px;\n`
+        // }
+        // if (node.parent && node.parent.type === 'FRAME' && node.parent.layoutMode === 'VERTICAL') {
+        //   text += `  margin: 0px ${node.parent.itemSpacing}px;\n`
+        // }
         // Auto layout
         if (node.layoutMode === 'HORIZONTAL') {
             text += `  display: flex;\n`;
@@ -118,6 +116,12 @@ function styledComponent(node) {
             text += `  display: flex;\n`;
             text += `  flex-direction: column;\n`;
             text += `  align-content: space-between;\n`;
+        }
+        else {
+            // if not auto layout, we set specific width and height
+            text += `  display: flex;\n`;
+            text += `  width: ${node.width}px;\n`;
+            text += `  height: ${node.height}px;\n`;
         }
         switch (node.layoutAlign) {
             case 'MIN':
@@ -132,26 +136,16 @@ function styledComponent(node) {
         }
     }
     else if (node.type === 'TEXT') {
-        // font-family: Roboto;
-        // font-style: normal;
-        // font-weight: normal;
-        // font-size: 12px;
-        // line-height: 14px;
-        // color: #000000;
-        // /* Inside Auto Layout */
-        // flex: none;
-        // order: 1;
-        // align-self: center;
-        // margin: 6px 0px;
         const ALIGNVERTICAL = {
-            CNETER: 'center',
+            CNETER: 'middle',
             TOP: 'flex-start',
             BOTTOM: 'flex-end',
         };
         const ALIGNHORIZONTAL = {
             CNETER: 'center',
-            TOP: 'flex-start',
-            BOTTOM: 'flex-end',
+            LEFT: 'left',
+            RIGHT: 'right',
+            JUSTIFIED: 'justify',
         };
         const fontName = (node.fontName);
         // const lineHeight = <LineHeight>(node.lineHeight)
@@ -159,9 +153,16 @@ function styledComponent(node) {
         // text += node.lineHeight ? `  line-height: ${<LineHeight>(node.lineHeight)}px;\n`: ''
         text += fontName.family ? `  font-family: ${fontName.family};\n` : '';
         text += fontName.style ? `  font-weight: ${fontName.style};\n` : '';
-        text += node.textAlignHorizontal ? `  text-align: ${ALIGNVERTICAL[node.textAlignHorizontal]};\n` : '';
-        text += node.textAlignVertical ? `  algin-items: ${ALIGNHORIZONTAL[node.textAlignVertical]};\n` : '';
+        text += ALIGNHORIZONTAL[node.textAlignHorizontal] ? `  text-align: ${ALIGNHORIZONTAL[node.textAlignHorizontal]};\n` : '';
+        text += ALIGNVERTICAL[node.textAlignVertical] ? `  vertical-align: ${ALIGNVERTICAL[node.textAlignVertical]};\n` : '';
         // text += node.letterSpacing ? `  letter-spacing: ${<LetterSpacing>(node.letterSpacing)}em;\n`: ''
+        // Auto layout
+        // if not auto layout, we set specific width and height
+        text += `  position: relative;\n`;
+        text += `  left: ${node.x}px;\n`;
+        text += `  top: ${node.y}px;\n`;
+        text += `  width: ${node.width}px;\n`;
+        text += `  height: ${node.height}px;\n`;
         // figma.loadFontAsync(node.getRangeFontName(0, node.characters.length - 1))
         // .then(font => {
         // })
@@ -186,18 +187,11 @@ function extractNodes(node) {
 figma.currentPage.selection.forEach(node => {
     extractNodes(node);
 });
-// for (let i = 0; i < figma.currentPage.selection.length; i++) {
-//   extractNodes(figma.currentPage.selection[i])
-// }
-// console.log('allNodes:', allNodes)
-// console.log('// Styled components')
-totalText += '// Styled components\n';
-allNodes.forEach(node => {
-    const text = styledComponent(node);
-    // console.log('  --- ')
-    // console.log(text)
-    totalText += text + '\n';
-});
+// totalText += '// Styled components\n'
+// allNodes.forEach(node => {
+//   const text = styledComponent(node)
+//   totalText += text + '\n'
+// })
 function levelTab(level) {
     let tab = '';
     for (let i = 0; i < level; i++) {
@@ -206,9 +200,6 @@ function levelTab(level) {
     return tab;
 }
 let Svgs = '';
-// <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-// <path d="M12.9725 4.63002C12.7025 4.24752 12.2525 4.00002 11.75 4.00002L3.5 4.00752C2.675 4.00752 2 4.67502 2 5.50002V13C2 13.825 2.675 14.4925 3.5 14.4925L11.75 14.5C12.2525 14.5 12.7025 14.2525 12.9725 13.87L16.25 9.25002L12.9725 4.63002Z" fill="#C92892"/>
-// </svg>
 // if a frame has vector children, assuming that is svg
 function extractJsx(node, level) {
     // check SVG
@@ -251,15 +242,31 @@ function extractJsx(node, level) {
     return text;
 }
 // console.log('// JSX')
-totalText += '// functional component\n';
+// totalText += '// functional component\n'
 figma.currentPage.selection.forEach(node => {
     const text = extractJsx(node, 2);
+    // totalText += '// Styled components\n'
+    let styledComponents = '';
+    allNodes.forEach(node => {
+        const text = styledComponent(node);
+        if (text) {
+            styledComponents += text + '\n';
+        }
+    });
     const jsx = `
-function Component(props: Props) {
+import React from 'react'
+import styled from 'styled-components'
+
+function ${node.name}Component() {
   return (
 ${text}
   )
 }
+
+// Styled components
+${styledComponents}
+
+export default ${node.name}Component
 `;
     // console.log(jsx)
     totalText += jsx + '\n';
